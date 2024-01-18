b960c58ec7b8880c3f064ee22b6885a0b18a7b79
Alex Wright <wrighal999@gmail.com>
2024-01-18 16:02:59 -0600

    Update Reactor

--- reactor.lua
+++ reactor.lua
@@ -1,11 +1,7 @@
-local NumberOfNeutrons
+local NumberOfNeutrons -- Power now
 local MaxNeutrons = 100000000000 -- 10e11
 local ReactorPower = NumberOfNeutrons / MaxNeutrons
-
-local NumberOfNeutrons -- Power now
-
 local oldNumberOfNeutrons -- Power in previous timestep
-
 local SomeFactors
 local IdleNeutrons = 1000
 
@@ -19,3 +15,8 @@ local ReactorPeriod
 local TimeStep -- number of seconds
 
 ReactorPeriod = 1 / math.log( NumberOfNeutrons / oldNumberOfNeutrons) * 1 / TimeStep
+
+local WaterDensity = 1000
+
+SomeFactors = 2 * ( 0.2 + RodsPulled * 0.8 / 100 ) * WaterDensity / 1000
+
